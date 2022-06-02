3895b96707c4efb622c0e9e0474edb9703dc4e61
KevinBui33 <kevin.bui709@gmail.com>
2022-06-02 12:54:36 -0400

    comments

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -19,6 +19,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
+// TODO: Make error message disappear when inputting text field
 const theme = createTheme();
 
 function Login() {
